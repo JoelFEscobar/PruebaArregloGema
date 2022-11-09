5d4f6160bf5094c32af2d6faa201831ded5a32a1
Joel F. Escobar Socas <alu0101130408@ull.edu.es>
2022-11-09 22:43:15 +0000

    Estructura inicial de la gema creada

---
+++ lib/Parking.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Parking/version"
+
+module Parking
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/Parking_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Parking do
+  it "has a version number" do
+    expect(Parking::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
