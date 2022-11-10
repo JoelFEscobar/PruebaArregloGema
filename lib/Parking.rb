3477fc6ff51322884dcad86ddf08a84f915cbaf0
Joel F. Escobar Socas <alu0101130408@ull.edu.es>
2022-11-10 09:33:23 +0000

    prueba del modulo aparcamiento falla

--- lib/Parking.rb
+++ lib/Parking.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "Parking/version"
+require_relative "Parking/aparcamiento"
 
 module Parking
   class Error < StandardError; end
---
+++ spec/Aparcamiento_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require "spec_helper.rb"
+RSpec.describe Parking do
+  describe Aparcamiento do
+    # context "Pruebas del módulo Aparcamiento" do
+    #   it "Existe un módulo para representar información del Aparcamiento" do
+    #     expect(Aparcamiento).not_to eq(nil)
+    #   end
+    end
+end
