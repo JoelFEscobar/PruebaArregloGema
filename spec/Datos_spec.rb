693c197a7c97586955eea84ffde58fc713b0e452
Joel F. Escobar Socas <alu0101130408@ull.edu.es>
2022-11-10 10:16:00 +0000

    fallan las pruebas de la clase datos

--- spec/Datos_spec.rb
+++ spec/Datos_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe Parking do
   describe Datos do
     context "Pruebas de la clase Datos" do
 
+      before :all do
+        @park1 = Datos.new(3,5,40, "Parking1", "Mixto", "coches", 30, 30, [2,2,2])
+      end
+      it "Pruebas de instancia de la clase Datos" do 
+        expect(@park1).not_to eq(nil)
+        #expect(@park1.plazas.to_s).not_to eq([2, 2, 2])
+      end
+
+
+
     end
   end
 end
