dce411c6e92558f76ea53227138dc743825c69cd
Joel F. Escobar Socas <alu0101130408@ull.edu.es>
2022-11-10 10:18:18 +0000

    la clase funciona

--- lib/Parking/datos.rb
+++ lib/Parking/datos.rb
@@ -1,9 +1,54 @@
 include Aparcamiento
 
 
+
 class Datos
+  attr_reader :accesibilidad, :seguridad, :identificador, :nombre, :descripcion, :tipo, :estado, :plazas
+  
+  def initialize(accesibilidad, seguridad, identificador, nombre, descripcion, tipo, libre, ocupado)
+    @accesibilidad = Accesibilidad(accesibilidad)
+    @seguridad = Seguridad(seguridad)
+    @id = identificador
+    @nombre_comercial = nombre
+    @descripcion = TipoDescripcion(descripcion)
+    @tipo = TipoAparcamiento(tipo)
+    #@plazas = PlazasAparcamiento::CaracterisiticasPlazas.new(array)
+    aux = [Aparcamiento.libres(libre),Aparcamiento.ocupadas(ocupado)]
+    @estado = Aparcamiento.EstadoPlazas()
+  end
 
+  def TipoDescripcion(desc)
+    if (desc == "Cubierto" || desc == "Aire Libre" || desc == "Mixto") then
+      return desc
+    else  
+      return nil
+    end
+  end
 
+  def TipoAparcamiento(tipo)
+    if (tipo == "autobuses" || tipo == " bicicletas" || tipo == "coches" || tipo == "motos" ) then
+      return tipo
+    else  
+      return nil
+    end
+  end
 
+  def Accesibilidad(accesibilidad) 
+    range = (1..5).to_a
+    if (range.include?(accesibilidad))
+      return accesibilidad
+    else
+      return nil
+    end
+  end
+
+  def Seguridad(seg) 
+    range = (1..10).to_a
+    if (range.include?(seg))
+      return seg
+    else
+      return nil
+    end
+  end
 
 end
--- spec/Datos_spec.rb
+++ spec/Datos_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Parking do
     context "Pruebas de la clase Datos" do
 
       before :all do
-        @park1 = Datos.new(3,5,40, "Parking1", "Mixto", "coches", 30, 30, [2,2,2])
+        @park1 = Datos.new(3,5,40, "Parking1", "Mixto", "coches", 30, 30)
       end
       it "Pruebas de instancia de la clase Datos" do 
         expect(@park1).not_to eq(nil)
